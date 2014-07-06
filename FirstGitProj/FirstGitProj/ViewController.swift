337b948e7b13348ae9d79227f099d0da2e7a736b
wbjames <314771755@qq.com>
2014-07-06 22:02:17 +0800

    xcode commit

--- FirstGitProj/FirstGitProj/ViewController.swift
+++ FirstGitProj/FirstGitProj/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
+        let b = 123
         // Do any additional setup after loading the view, typically from a nib.
     }
 
